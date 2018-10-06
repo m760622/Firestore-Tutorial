2d96f804e9ab260edabe9d6e82eeb40c6f4883d9
ula1990 <uladzislau.daratsiuk@gmail.com>
2018-10-06 10:18:13 +0200

    Initial Commit

---
+++ Firestore Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firestore Tutorial
+//
+//  Created by Uladzislau Daratsiuk on 10/6/18.
+//  Copyright © 2018 Uladzislau Daratsiuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
